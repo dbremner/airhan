e74faed8d56b1a5b8b35a74c41547202ba14faed
lianghancn <lianghancn@29280b2a-3d39-11de-8f11-b9fadefa0f10>
2009-05-11 13:27:55 +0000

    Insertion sort impl

--- trunk/msvc/air/application/main.cpp
+++ trunk/msvc/air/application/main.cpp
@@ -1,35 +1,48 @@
+// TODO - swtich to use Google Test Framework
+
 #include <iostream>
 
 #include "sort/MergeSort.h"
 #include "sort/BubbleSort.h"
+#include "sort/InsertionSort.h"
+
 using namespace lianghancn::air::sort;
 
-// temp test file - need to use gtest asap..
+#define LH_SORT_RAT int a[16] = {-1, 2, 10, 9, 8, 7, 0, 0, 3, 11, 15, 20, -90, 100, 200, -1}
+
+#define LH_PRINT_SORT_RESULT \
+   for (int i = 0; i < 16; i ++) \
+    { \
+        std::cout<<a[i]<<" "; \
+    }\
+	std::cout<<std::endl 
+
 
 void merge_sort()
 {
-    int a[16] = {-1, 2, 10, 9, 8, 7, 0, 0, 3, 11, 15, 20, -90, 100, 200, -1};
+	LH_SORT_RAT;
+    
     MergeSort(a, 16, false);
 
-    for (int i = 0; i < 16; i ++)
-    {
-        std::cout<<a[i]<<",";
-    }
-
-    std::cout<<std::endl;
+	LH_PRINT_SORT_RESULT;
 }
 
 void bubble_sort()
 {
-    int a[16] = {-1, 2, 10, 9, 8, 7, 0, 0, 3, 11, 15, 20, -90, 100, 200, -1};
+	LH_SORT_RAT;
+
     BubbleSort(a, 16);
 
-    for (int i = 0; i < 16; i ++)
-    {
-        std::cout<<a[i]<<",";
-    }
+	LH_PRINT_SORT_RESULT;
+}
 
-    std::cout<<std::endl;
+void insertion_sort()
+{
+	LH_SORT_RAT;
+
+	BubbleSort(a, 16);
+
+	LH_PRINT_SORT_RESULT;
 }
 
 int main()
@@ -37,4 +50,6 @@ int main()
    merge_sort();
 
    bubble_sort();
+
+   insertion_sort();
 }
