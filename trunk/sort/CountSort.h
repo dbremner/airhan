946d7df7b6a88a1914e37b4563483ca56b99dfa2
lianghancn <lianghancn@29280b2a-3d39-11de-8f11-b9fadefa0f10>
2009-05-12 14:47:55 +0000

    count sort - note there is an intentional bug in left in it for future lessons : -)

---
+++ trunk/sort/CountSort.h
@@ -0,0 +1,71 @@
+/***************************************************************
+
+Copyright (c) 2008 Michael Liang Han
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+****************************************************************/
+
+namespace lianghancn
+{
+	namespace air
+	{
+		namespace sort
+		{
+			// it only works for positive integer array :- )
+			template<typename T> void CountSort(T a[], int n)
+			{
+				T* pCount = new T[256];
+				T* pOld = new T[n];
+					
+				// initialize count array
+				for (int i = 0; i < 256; i ++)
+				{
+					pCount[i] = 0;
+				}
+				
+				// copy original array and 1st pass over count array
+				for (int i = 0; i < n; i ++)
+				{					
+					pOld[i] = a [i];
+					pCount[a[i]] ++;
+				}
+
+				// 2nd pass over count array to make it accumulative
+				for (int i = 1; i < n; i ++)
+				{
+					pCount[i] = pCount[i - 1] + pCount[i];
+				}
+
+				// final pass over both array to directly put element to its deserved position
+				for (int i = 0; i < n; i ++)
+				{
+					a[pCount[pOld[i]]] = pOld[i];
+					pCount[pOld[i]] -- ;
+				}
+
+				delete[] pCount;
+				delete[] pOld;
+			}
+		};
+	};
+};
